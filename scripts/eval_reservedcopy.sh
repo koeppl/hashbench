93e844abf73efe7dfca97914f5a2dd86acb2fbde
Marvin Löbel <loebel.marvin@gmail.com>
2019-03-22 16:04:39 +0100

    fix reservedcopy not being called

--- scripts/eval_reservedcopy.sh
+++ scripts/eval_reservedcopy.sh
@@ -9,7 +9,7 @@ jsonfile=$(mktemp)
 
 while [[ $size -le 268435456 ]]; do
 	set -x
-	../build/randomcopy "$size" > "$jsonfile"
+	../build/reservecopy "$size" > "$jsonfile"
 	set +x
 	./readjson.sh "$jsonfile" |
 		while read -r line; do
